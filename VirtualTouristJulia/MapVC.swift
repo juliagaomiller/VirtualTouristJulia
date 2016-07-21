adbe0a1317822f85efb89501efbd85db109cf6dd
Julia Miller <julia.gao.miller@gmail.com>
2016-07-21 10:41:11 -0700

    am able to delete pins on map. Now have to update NSUserDefaults.

--- VirtualTouristJulia/MapVC.swift
+++ VirtualTouristJulia/MapVC.swift
@@ -16,61 +16,107 @@ class MapVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var deleteView: UIView!
     @IBOutlet weak var deleteLabel: UILabel!
     
-    var editBtn: UIBarButtonItem!
+    @IBOutlet weak var editBtn: UIBarButtonItem!
+    //var editBtn: UIBarButtonItem!
     var doneBtn: UIBarButtonItem!
+    var locationArray = [[String: Double]]()
     var deleteMode = false
     
     override func viewDidLoad() {
-        prepareView()
-    }
-    
-    func prepareView(){
-        
-        let longPress = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
-        longPress.minimumPressDuration = 1.5
-        map.addGestureRecognizer(longPress)
-        
-        editBtn = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: "enableDelete")
-        doneBtn = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "disableDelete")
+        map.delegate = self
         
         disableDelete()
         
-        //LOAD UP MAP LOCATION FROM NSUSERDEFAULTS
+        if NSUserDefaults.standardUserDefaults().objectForKey("LocationArray") != nil {
+            locationArray = NSUserDefaults.standardUserDefaults().objectForKey("LocationArray") as! [[String:Double]]
+            for dictionary in locationArray {
+                addAnnotation(dictionary["Latitude"]!, long: dictionary["Longitude"]!)
+            }
+        }
         
-        //LOAD UP ARRAY OF PINS
+        let longPress = UILongPressGestureRecognizer(target: self, action: "getTouchCoord:")
+        longPress.minimumPressDuration = 1.5
+        map.addGestureRecognizer(longPress)
+        
+        //editBtn = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: "enableDelete")
+        doneBtn = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "disableDelete")
+        
+        //LOAD UP MAP LOCATION FROM NSUSERDEFAULTS
     }
     
-    func addAnnotation(touch: UIGestureRecognizer){
+    func getTouchCoord(touch: UIGestureRecognizer){
         if touch.state == UIGestureRecognizerState.Began {
             let point = touch.locationInView(map)
-            print("addAnnotation:", point)
             let coordinate = map.convertPoint(point, toCoordinateFromView: map)
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = coordinate
-            self.map.addAnnotation(annotation)
+            
+            let locationDictionary = ["Latitude" : coordinate.latitude, "Longitude" : coordinate.longitude]
+            
+            locationArray.append(locationDictionary)
+            
+            NSUserDefaults.standardUserDefaults().setObject(locationArray, forKey: "LocationArray")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            addAnnotation(coordinate.latitude, long: coordinate.longitude)
         }
     }
     
-    func enableDelete(){
+    func addAnnotation(lat: CLLocationDegrees, long: CLLocationDegrees){
+        CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: lat, longitude: long), completionHandler: { (placemark, error) -> Void in
+            if error != nil {
+                print("Reverse Geocoder Error")
+                return
+            }
+            if placemark!.count > 0 {
+                let pm = placemark![0] as CLPlacemark
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2DMake(lat, long)
+                annotation.title = "\(pm.locality!), \(pm.administrativeArea!)"
+
+                self.map.addAnnotation(annotation)
+                
+            }
+        })
+    }
+    
+    @IBAction func enableDelete(sender: AnyObject) {
         deleteMode = true
         deleteLabel.hidden = false
         deleteView.hidden = false
         self.navigationItem.rightBarButtonItem = doneBtn
     }
     
-    func disableDelete(){
+    func disableDelete() {
         deleteMode = false
         deleteLabel.hidden = true
         deleteView.hidden = true
         self.navigationItem.rightBarButtonItem = editBtn
     }
     
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-        print("Tapped the pin!")
-        if (deleteMode) {
-            self.map.removeAnnotation(view.annotation!)
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        if (!deleteMode){
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        }
+        return annotationView
+    }
+
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if (!deleteMode){
+            print("Segue into AlbumVC and pass view.title")
         }
     }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        if (deleteMode) {
+            print(view.annotation)
+            self.map.removeAnnotation(view.annotation!)
+        }
+//        for annotationDic in locationArray {
+//            
+//        }
+    }
+
     
 }
 
