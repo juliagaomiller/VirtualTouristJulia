3dd4ca34d4f04485f5e63d3ec580b25456b471dc
Julia Miller <julia.gao.miller@gmail.com>
2016-07-27 13:23:58 -0700

    The array in NSUserDefaults is no longer saving the pin arrays strangely. Now I need to create the Pin file and be able to save an array of them in Core Data instead of NSUserdefaults. The Pin file will also have an array of Pictures.

--- VirtualTouristJulia/MapVC.swift
+++ VirtualTouristJulia/MapVC.swift
@@ -4,7 +4,13 @@
 //
 //  Created by Julia Miller on 7/19/16.
 //  Copyright © 2016 Julia Miller. All rights reserved.
-//
+
+//PTD NEXT TIME: CREATE THE PIN.SWIFT FILE AND HAVE SO THAT THE ARRAY OF PIN[] IS SAVED IN CORE DATA 
+//EVERY TIME A PIN IS PLACED, YOU SEND A REQUEST TO SAVE 9 IMAGES FROM FLICKR
+//REREAD REQUIREMENTS
+//TRY TO DUPLICATE CODE FROM OTHER PROJECTS AND EXPLAIN WHY IT WORKS. 
+//SAVE PIN IN CORE DATA.
+//RELOOK AT NOTES.
 
 import Foundation
 import UIKit
@@ -17,9 +23,8 @@ class MapVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var deleteLabel: UILabel!
     
     @IBOutlet weak var editBtn: UIBarButtonItem!
-    //var editBtn: UIBarButtonItem!
     var doneBtn: UIBarButtonItem!
-    var locationArray = [[String: Double]]()
+    var locationArray = [[String: String]]()
     var deleteMode = false
     
     override func viewDidLoad() {
@@ -27,55 +32,68 @@ class MapVC: UIViewController, MKMapViewDelegate {
         
         disableDelete()
         
-        if NSUserDefaults.standardUserDefaults().objectForKey("LocationArray") != nil {
-            locationArray = NSUserDefaults.standardUserDefaults().objectForKey("LocationArray") as! [[String:Double]]
+        if NSUserDefaults.standardUserDefaults().objectForKey("locationArray") != nil {
+            locationArray = NSUserDefaults.standardUserDefaults().objectForKey("locationArray") as! [[String:String]]
             for dictionary in locationArray {
-                addAnnotation(dictionary["Latitude"]!, long: dictionary["Longitude"]!)
+                let lat = Double(dictionary["Latitude"]!)
+                let long = Double(dictionary["Longitude"]!)
+                let title = dictionary["LocName"]
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2DMake(lat!, long!)
+                annotation.title = title
+                
+                self.map.addAnnotation(annotation)
             }
         }
         
-        let longPress = UILongPressGestureRecognizer(target: self, action: "getTouchCoord:")
-        longPress.minimumPressDuration = 1.5
+        let longPress = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+        longPress.minimumPressDuration = 1
         map.addGestureRecognizer(longPress)
         
-        //editBtn = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: "enableDelete")
         doneBtn = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "disableDelete")
         
-        //LOAD UP MAP LOCATION FROM NSUSERDEFAULTS
+        //PTD - Load up map location from previous time map was closed.
     }
     
-    func getTouchCoord(touch: UIGestureRecognizer){
-        if touch.state == UIGestureRecognizerState.Began {
-            let point = touch.locationInView(map)
-            let coordinate = map.convertPoint(point, toCoordinateFromView: map)
-            
-            let locationDictionary = ["Latitude" : coordinate.latitude, "Longitude" : coordinate.longitude]
-            
-            locationArray.append(locationDictionary)
-            
-            NSUserDefaults.standardUserDefaults().setObject(locationArray, forKey: "LocationArray")
-            NSUserDefaults.standardUserDefaults().synchronize()
-            
-            addAnnotation(coordinate.latitude, long: coordinate.longitude)
+    func dropPin(touch: UIGestureRecognizer){
+        if (!deleteMode){
+            if touch.state == UIGestureRecognizerState.Began {
+                let point = touch.locationInView(map)
+                let coordinate = map.convertPoint(point, toCoordinateFromView: map)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude)
+                
+                let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemark, error) -> Void in
+                    if error != nil {
+                        print("MapVC59: Reverse Geocoder Error")
+                        return
+                    }
+                    if placemark!.count > 0 {
+                        let pm = placemark![0] as CLPlacemark
+                        if pm.administrativeArea != nil && pm.locality != nil {
+                            annotation.title = "\(pm.locality!), \(pm.administrativeArea!)"
+                        } else {
+                            annotation.title = "Unknown Location"
+                            print("MapVC68: Pin is outside of U.S.")
+                            //PTD - print pm and check where ocean is located
+                            //PTD - check pm and check how to write city and country of foreign country
+                        }
+                        self.savePin(String(coordinate.latitude), long: String(coordinate.longitude), title: annotation.title!)
+                    }
+                })
+                self.map.addAnnotation(annotation)
+            }
         }
     }
     
-    func addAnnotation(lat: CLLocationDegrees, long: CLLocationDegrees){
-        CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: lat, longitude: long), completionHandler: { (placemark, error) -> Void in
-            if error != nil {
-                print("Reverse Geocoder Error")
-                return
-            }
-            if placemark!.count > 0 {
-                let pm = placemark![0] as CLPlacemark
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = CLLocationCoordinate2DMake(lat, long)
-                annotation.title = "\(pm.locality!), \(pm.administrativeArea!)"
-
-                self.map.addAnnotation(annotation)
+    func savePin(lat: String, long: String, title: String){
+        let locationDictionary = ["Latitude" : lat, "Longitude" : long, "LocName" : title]
+        self.locationArray.append(locationDictionary)
                 
-            }
-        })
+        NSUserDefaults.standardUserDefaults().setObject(self.locationArray, forKey: "locationArray")
+        NSUserDefaults.standardUserDefaults().synchronize()
     }
     
     @IBAction func enableDelete(sender: AnyObject) {
@@ -90,6 +108,8 @@ class MapVC: UIViewController, MKMapViewDelegate {
         deleteLabel.hidden = true
         deleteView.hidden = true
         self.navigationItem.rightBarButtonItem = editBtn
+        //print("MapVC105: The right bar button item is: ", self.navigationItem.rightBarButtonItem?.title)
+        //PTD - Figure out why the edit button isn't showing back up again when you press Done.
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
@@ -97,7 +117,7 @@ class MapVC: UIViewController, MKMapViewDelegate {
         if (!deleteMode){
             annotationView.canShowCallout = true
             annotationView.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
-        }
+        } else { annotationView.canShowCallout = false }
         return annotationView
     }
 
@@ -107,39 +127,31 @@ class MapVC: UIViewController, MKMapViewDelegate {
         }
     }
     
+    //CURRENTLY THE APP WILL CRASH BECAUSE I DON'T HAVE THE CODE BELOW FIGURED OUT. AM TRYING OT FIGURE OUT HOW TO GET THE INDEX OF DICTIONARY
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
         if (deleteMode) {
-            
-            let lat = view.annotation!.coordinate.latitude
-            let long = view.annotation!.coordinate.longitude
-            
-            print("Before delete: ", locationArray)
-            
-            for annotationDic in locationArray {
-                var i = 0
-                if lat == annotationDic["Latitude"]{
-                    if long == annotationDic["Longitude"]{
-                        print("index: ", i)
-                        print(locationArray[i])
-                        locationArray.removeAtIndex(i)
-                        NSUserDefaults.standardUserDefaults().setObject(locationArray, forKey: "LocationArray")
-                        NSUserDefaults.standardUserDefaults().synchronize()
-                    self.map.removeAnnotation(view.annotation!)
-                        print("After delete: ", locationArray)
-                        print("NSUserdefault: ", NSUserDefaults.standardUserDefaults().objectForKey("LocationArray"))
-                        return
-                    }
+            guard let selectedTitle = view.annotation!.title!
+                else {
+                    print("MapVC126-Error.")
+                    return
                 }
-                i++
+            for (index, _) in locationArray.enumerate() {
+                let title = locationArray[index]["LocName"]
+                if selectedTitle == title!{
+                    print("Index: ", index)
+                    print(title)
+                    locationArray.removeAtIndex(index)
+                    NSUserDefaults.standardUserDefaults().setObject(locationArray, forKey: "locationArray")
+                    NSUserDefaults().synchronize()
+                    self.map.removeAnnotation(view.annotation!)
+                    return
+                }
             }
-            
-            
         }
-//        for annotationDic in locationArray {
-//            
-//        }
     }
 
-    
 }
+
+    
+
 
